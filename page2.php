17c8a75a2315088b872c43c6c89598b588030825
TattieMasher <alexandermccaughran@yahoo.com>
2024-10-29 20:08:11 +0000

    began second page

---
+++ page2.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Playfair+Display:ital,wght@0,500;1,500&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script> <!-- Confetti -->
+    </head>
+    <body>
+        <?php include('nav.php'); ?>
+        <main>
+          <div class="wedding-grid-main">
+            <img class="img-eiffel" src="PXL_20240912_124742216.jpg"/>
+            <div>
+              <div class="wedding-desc-main">
+                <div>
+                  <p>
+                    Our wedding will be held on Friday, 1st August, 2025 at 2pm. 
+                  </p>
+                  <p>
+                    After our wedding, please join us for some lovely photos in the picturesque Kay Park before enjoying a three-course meal.
+                  </p>
+                </div>
+                  <div>
+                    <i>
+                      <p>
+                        <b>No dress code.</b>
+                      </p>
+                      <p>
+                        Wear what makes you feel best!
+                      </p>
+                    </i>
+                </div>
+              </div>
+              <div class="wedding-detail-grid">
+                <div class="testtesttest">
+                  <h3 class="wedding-detail-title">ARRIVAL  </h3>
+                  <h5 class="wedding-detail-title-time"> 1:30PM</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+    </body>
+</html>
