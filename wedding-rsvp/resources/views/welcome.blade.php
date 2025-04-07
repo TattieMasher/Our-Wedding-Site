e3a272a7fc4a005feb0d6eb3fd1ee75faef0e2b9
Alex McCaughran <alexmccaughran@ymail.com>
2025-04-07 15:57:48 +0100

    Started site content

--- wedding-rsvp/resources/views/welcome.blade.php
+++ wedding-rsvp/resources/views/welcome.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Alex & Candace')
 
 @section('content')
+    <h1>Welcome to Our Wedding!</h1>
 
-Oor weeding
+    <main>
+        <header>
+            <h1 class="header-hearts">
+                ❤❤
+            </h1>
+            <h1 class="main-header">
+                Alex M<sup>c</sup>Caughran
+                <br> — and — <br>
+                Candace Scoular
+            </h1>
+            <div class="index-details-block">
+                FRIDAY 1ˢᵗ AUGUST 2025<br>
+                SANTINO KILMARNOCK<br>
+                Arrival by 1:30pm<br>
+            </div>
+        </header>
+        <div class="index-RSVP-invitation">
+        We request the pleasure of your company as we celebrate our love.<br><br>
+        <a class="RSVP-invite-link" href="">Please let us know if you can attend (RSVP)</a>
+        </div>
+    </main>
+@endsection
