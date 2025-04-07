722eccb49bed63fa77a9333de86ee44eef0b320b
Alex McCaughran <alexmccaughran@ymail.com>
2025-04-07 21:55:55 +0100

    Adde ConfettiJS

--- wedding-rsvp/resources/views/welcome.blade.php
+++ wedding-rsvp/resources/views/welcome.blade.php
@@ -3,6 +3,43 @@
 @section('title', 'Alex & Candace')
 
 @section('content')
+    <script>
+        const duration = 3 * 1000,
+        animationEnd = Date.now() + duration;
+
+        let skew = 1;
+
+        function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+        }
+
+        (function frame() {
+        const timeLeft = animationEnd - Date.now(),
+            ticks = Math.max(200, 500 * (timeLeft / duration));
+
+        skew = Math.max(0.8, skew - 0.001);
+
+        confetti({
+            particleCount: 1,
+            startVelocity: 0,
+            ticks: ticks,
+            origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: Math.random() * skew - 0.2,
+            },
+            colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+            shapes: ["heart"],
+            gravity: randomInRange(0.4, 0.6),
+            scalar: randomInRange(2, 4),
+            drift: randomInRange(-0.4, 0.4),
+        });
+
+        if (timeLeft > 0) {
+            requestAnimationFrame(frame);
+        }
+        })();
+    </script>
     <section class="hero">
         <h1 class="hero-hearts">❤❤</h1>
 
