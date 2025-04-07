9dda8f8195e15853f9b4e4d6791a2b0a61ab0932
Alex McCaughran <alexmccaughran@ymail.com>
2025-04-07 21:32:15 +0100

    Nicer navbar

--- wedding-rsvp/resources/views/partials/navbar.blade.php
+++ wedding-rsvp/resources/views/partials/navbar.blade.php
@@ -1,8 +1,16 @@
 <nav class="navbar">
-    <a href="{{ route('home') }}" class="navbar-brand">A & C</a>
-    <ul class="navbar-links">
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="#info">Info</a></li>
-        <li><a href="#rsvp">RSVP</a></li>
-    </ul>
+    <div class="navbar-inner">
+        <a href="{{ route('home') }}" class="navbar-brand">A & C</a>
+
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <label for="nav-toggle" class="nav-toggle-label">
+            <span></span>
+        </label>
+
+        <ul class="navbar-links">
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="#info">Info</a></li>
+            <li><a href="#rsvp">RSVP</a></li>
+        </ul>
+    </div>
 </nav>
