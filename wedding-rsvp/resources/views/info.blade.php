9768ded91d0c8aa50397fcb94bc40201566817bc
Alex McCaughran <alexmccaughran@ymail.com>
2025-05-08 23:03:52 +0100

    Bunchastuff

--- wedding-rsvp/resources/views/info.blade.php
+++ wedding-rsvp/resources/views/info.blade.php
@@ -6,7 +6,7 @@
 <section class="wedding-info">
     <div class="info-grid">
         <div class="info-photo">
-            <img src="/img/eiffel.jpg" alt="Alex & Candace at Eiffel Tower">
+            <img src="/img/eiffel-sm.jpg" alt="Alex & Candace at Eiffel Tower">
         </div>
         <div class="info-text">
             <p>Our wedding will be held on <strong><u>Friday, 1st August, 2025 at 2pm</u></strong>.</p>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="venue-box">
-        <h3>ARRIVAL BY 1:30PM</h3>
+        <h3>ARRIVAL BY 1:00PM</h3>
         <p>Fenwick Hotel</p>
         <p>Fenwick</p>
         <p>KA3 6AU</p>
@@ -26,7 +26,7 @@
     <div class="schedule-grid">
         <div>
             <h4>Ceremony</h4>
-            <p>2:00PM – 3:00PM</p>
+            <p>1:30PM – 3:00PM</p>
         </div>
         <div>
             <h4>Photos</h4>
@@ -36,6 +36,15 @@
             <h4>Meal / Reception</h4>
             <p>4:00PM – Late!</p>
         </div>
+    </div>
+
+    <div class="gift-info">
+        <p>
+            Your presence is the best gift we could ask for!
+            We've got all the “stuff” we need,
+            but if you'd like to contribute to our wedding fund, we'd be so grateful (no pressure, of course!). 💕
+        </p>
+        <a href="#">Gift here</a>
     </div>
 </section>
 @endsection
--- wedding-rsvp/resources/views/rsvp/form-fields.blade.php
+++ wedding-rsvp/resources/views/rsvp/form-fields.blade.php
@@ -6,32 +6,50 @@
     @csrf
 
     @foreach ($household->guests as $guest)
-        <div>
-            <h3>{{ $guest->name }}</h3>
-            <label>
-                <input type="radio" name="guest[{{ $guest->id }}][is_attending]" value="1"
-                    {{ $guest->is_attending === true ? 'checked' : '' }}> Attending
-            </label>
-            <label>
-                <input type="radio" name="guest[{{ $guest->id }}][is_attending]" value="0"
-                    {{ $guest->is_attending === false ? 'checked' : '' }}> Not Attending
-            </label>
-            <br>
-            <label>
-                Special Requests:
-                <textarea name="guest[{{ $guest->id }}][special_requests]">{{ $guest->special_requests }}</textarea>
-            </label>
-        </div>
-        <hr>
-    @endforeach
+    <div class="guest-section">
+        <h3>{{ $guest->name }}</h3>
 
-    <h2>Song Requests</h2>
-    @for ($i = 0; $i < 3; $i++)
-        <div>
-            <input type="text" name="song_requests[{{ $i }}][title]" placeholder="Song Title">
-            <input type="text" name="song_requests[{{ $i }}][artist]" placeholder="Artist (optional)">
-        </div>
-    @endfor
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][is_attending]" value="1"
+                {{ $guest->is_attending === true ? 'checked' : '' }}> Attending
+        </label>
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][is_attending]" value="0"
+                {{ $guest->is_attending === false ? 'checked' : '' }}> Not Attending
+        </label>
+
+        <label>Special Requests:</label>
+        <textarea name="guest[{{ $guest->id }}][special_requests]">{{ $guest->special_requests }}</textarea>
+
+        <label>Dietary Requirements/Allergies:</label>
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][diet]" value="1"
+                {{ $guest->is_attending === true ? 'checked' : '' }}> None
+        </label>
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][diet]" value="0"
+                {{ $guest->is_attending === false ? 'checked' : '' }}> Vegetarian
+        </label>
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][diet]" value="0"
+                {{ $guest->is_attending === false ? 'checked' : '' }}> Vegan
+        </label>
+        <label>
+            <input type="radio" name="guest[{{ $guest->id }}][diet]" value="0"
+                {{ $guest->is_attending === false ? 'checked' : '' }}> Other
+        </label>
+        <textarea name="guest[{{ $guest->id }}][dietary_requirements]">{{ $guest->dietary_requirements }}</textarea>
+    </div>
+
+    <div class="contact-section">
+        <h3>Contact Details</h3>
+        <label>Email Address (optional):</label>
+        <input type="email" name="contact_email" placeholder="your@email.com" required>
+        <label>Phone Number (optional):</label>
+        <input type="number" name="contact_email" placeholder="your phone number" required>
+    </div>
+
+@endforeach
 
     <button type="submit">Submit RSVP</button>
 </form>
