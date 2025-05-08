17310edb01b3542f12204e96b3a642ebff8b4c49
Alex McCaughran <alexmccaughran@ymail.com>
2025-05-08 23:10:37 +0100

    Saving shit

--- wedding-rsvp/resources/views/info.blade.php
+++ wedding-rsvp/resources/views/info.blade.php
@@ -39,10 +39,10 @@
     </div>
 
     <div class="gift-info">
+        <div class="house">🏠</div>
         <p>
             Your presence is the best gift we could ask for!
-            We've got all the “stuff” we need,
-            but if you'd like to contribute to our wedding fund, we'd be so grateful (no pressure, of course!). 💕
+            <br><br>We've got all the “stuff” we need, but if you'd like to contribute to our new home fund, we'd be so grateful (no pressure, of course!). 💕
         </p>
         <a href="#">Gift here</a>
     </div>
